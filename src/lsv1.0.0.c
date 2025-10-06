e96b2c688dfacfb6cdd1e0aa5dbd654f9d2415aa
Mu-Rizwan <mr0039747@gmail.com>
2025-10-06 15:40:44 -0400

    feat: add -l long listing feature (v1.1.0)

--- src/lsv1.0.0.c
+++ src/lsv1.0.0.c
@@ -14,26 +14,36 @@
 #include <string.h>
 #include <errno.h>
 #include <sys/stat.h>
+#include <pwd.h>
+#include <grp.h>
+#include <time.h>
 
 extern int errno;
 
 void do_ls(const char *dir);
+void do_long_listing(const char *path);
 
-int main(int argc, char const *argv[])
+int main(int argc, char *argv[])
 {
-    if (argc == 1)
-    {
-        do_ls(".");
+    int long_listing = 0;
+    int opt;
+    while ((opt = getopt(argc, argv, "l")) != -1) {
+    	if (opt == 'l') long_listing = 1;
     }
-    else
-    {
-        for (int i = 1; i < argc; i++)
-        {
-            printf("Directory listing of %s : \n", argv[i]);
-            do_ls(argv[i]);
-	    puts("");
-        }
+
+    if (optind == argc) {
+    	if (long_listing) do_long_listing(".");
+    	else do_ls(".");
+    } 
+    else {
+    	for (int i = optind; i < argc; i++) {
+        	printf("Directory listing of %s:\n", argv[i]);
+        	if (long_listing) do_long_listing(argv[i]);
+        	else do_ls(argv[i]);
+        	puts("");
+    	}
     }
+
     return 0;
 }
 
@@ -60,4 +70,48 @@ void do_ls(const char *dir)
     }
 
     closedir(dp);
+}
+
+void do_long_listing(const char *path) {
+    DIR *dir = opendir(path);
+    if (!dir) {
+        perror("opendir");
+        return;
+    }
+
+    struct dirent *entry;
+    while ((entry = readdir(dir)) != NULL) {
+        struct stat st;
+        char fullpath[1024];
+        snprintf(fullpath, sizeof(fullpath), "%s/%s", path, entry->d_name);
+        if (lstat(fullpath, &st) == -1)
+            continue;
+
+        // Permissions
+        printf( (S_ISDIR(st.st_mode)) ? "d" : "-");
+        printf( (st.st_mode & S_IRUSR) ? "r" : "-");
+        printf( (st.st_mode & S_IWUSR) ? "w" : "-");
+        printf( (st.st_mode & S_IXUSR) ? "x" : "-");
+        printf( (st.st_mode & S_IRGRP) ? "r" : "-");
+        printf( (st.st_mode & S_IWGRP) ? "w" : "-");
+        printf( (st.st_mode & S_IXGRP) ? "x" : "-");
+        printf( (st.st_mode & S_IROTH) ? "r" : "-");
+        printf( (st.st_mode & S_IWOTH) ? "w" : "-");
+        printf( (st.st_mode & S_IXOTH) ? "x" : "-");
+
+        // Links, owner, group, size, date, name
+        struct passwd *pw = getpwuid(st.st_uid);
+        struct group  *gr = getgrgid(st.st_gid);
+        char timebuf[32];
+        strftime(timebuf, sizeof(timebuf), "%b %d %H:%M", localtime(&st.st_mtime));
+
+        printf(" %ld %s %s %5ld %s %s\n",
+               (long)st.st_nlink,
+               pw ? pw->pw_name : "?",
+               gr ? gr->gr_name : "?",
+               (long)st.st_size,
+               timebuf,
+               entry->d_name);
+    }
+    closedir(dir);
 }
